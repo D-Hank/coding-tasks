52fa60936daf876197262f6074455c0b666959fd
DHANK <1330747842@qq.com>
2025-05-13 17:14:59 +0800

    Fixed probs in run.sh and upload new results

--- src/run.sh
+++ src/run.sh
@@ -5,5 +5,7 @@ docker run --runtime nvidia --gpus all \
     --ipc=host \
     vllm/vllm-openai:latest \
     --model Qwen/Qwen2.5-Coder-0.5B-Instruct \
-    --max_model_len 4096 \ # Should be greater than output token len
+    --max_model_len 4096 \
     --api_key "EMPTY"
+
+# max_model_len should be greater than output token len
